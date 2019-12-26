9ba38667dcb3e882dc3294a0c50ab81efb11cb69
rishi6215 <59086705+rishi6215@users.noreply.github.com>
2019-12-26 14:07:10 +0530

    just added a few lines of code

--- sample/src/com/git/practice/Test.java
+++ sample/src/com/git/practice/Test.java
@@ -4,7 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		String name = "Rajat";
+		String lName=new String("Roy");
 		System.out.println("name" + name);
+System.out.println("full name" + name+ " "+lName );
 
 	}
 }
